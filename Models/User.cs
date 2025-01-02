688605a6bf42428b446b97ec3e7334c6d2be29fb
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-02 12:09:12 -0300

    feat: add relaction post

--- Models/User.cs
+++ Models/User.cs
@@ -7,4 +7,5 @@ public class User
   public string? Email { get; set; }
   public string? Password { get; set; }
   public string? Role { get; set; }
+  public ICollection<Post>? Posts { get; set; }
 }
