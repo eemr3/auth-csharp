f90e5bee959c4a7746657ed6af42471a8e9766a3
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-02 12:53:13 -0300

    refactor: refactor userdto

---
+++ DTO/UserDTOResponse.cs
@@ -0,0 +1,9 @@
+namespace AuthBlog.DTO;
+public class UserDTOResponse
+{
+  public int UserId { get; set; }
+  public string? Name { get; set; }
+  public string? Email { get; set; }
+  public string? Role { get; set; }
+
+}
