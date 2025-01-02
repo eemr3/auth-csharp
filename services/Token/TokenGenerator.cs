88f800d88f716aa5a6d109a7794b5fcebeda8fa4
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-02 12:25:38 -0300

    feat: create service config Token

---
+++ services/Token/TokenGenerator.cs
@@ -0,0 +1,49 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AuthBlog.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthBlog.Services.Token;
+
+public class TokenGenerator
+{
+
+  private readonly IConfiguration _configuration;
+
+  public TokenGenerator(IConfiguration configuration)
+  {
+    _configuration = configuration;
+  }
+  public string Generator(User user)
+  {
+    string alternativeKey = "BnPVLnJNzGo/ZSGAkbDrqdn2+ptErwpwJPC5bfiv4F1ySwvW26ET71u+7A2SXjAY";
+    var secret = _configuration["Jwt:SecretKey"] ?? alternativeKey;
+    var expiresHours = 4;
+
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var tokenDescriptor = new SecurityTokenDescriptor()
+    {
+      Subject = AddClaims(user),
+      SigningCredentials = new SigningCredentials(
+        new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret)),
+        SecurityAlgorithms.HmacSha256Signature
+      ),
+      Expires = DateTime.Now.AddHours(expiresHours)
+    };
+
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+    return tokenHandler.WriteToken(token);
+  }
+
+  private ClaimsIdentity AddClaims(User user)
+  {
+    var claims = new ClaimsIdentity();
+
+    claims.AddClaim(new Claim(ClaimTypes.Email, user.Email!));
+    claims.AddClaim(new Claim(ClaimTypes.Name, user.Name!));
+    claims.AddClaim(new Claim(ClaimTypes.Role, user.Role!));
+
+    return claims;
+  }
+}
