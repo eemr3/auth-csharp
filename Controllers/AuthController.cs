385281ff6d77eed2adb06fb04cd7ede0690c5426
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-02 15:06:21 -0300

    feat: create controller Auth

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+using AuthBlog.DTO;
+using AuthBlog.Exceptions;
+using AuthBlog.Services.Token;
+using AuthBlog.Services.UserService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthBlog.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+  private readonly IUserService _userService;
+  private readonly TokenGenerator _tokenGenerator;
+
+  public AuthController(IUserService userService, IConfiguration configuration)
+  {
+    _userService = userService;
+    _tokenGenerator = new TokenGenerator(configuration);
+  }
+
+  [HttpPost("login")]
+  public async Task<IActionResult> Login([FromBody] LoginDTORequest loginRequest)
+  {
+    try
+    {
+      var user = await _userService.GetUserByEmailAsync(loginRequest.Email!);
+      if (user.Password != loginRequest.Password)
+      {
+        return Unauthorized(new { message = "Email address or password provided is incorrect." });
+      }
+      var Token = _tokenGenerator.Generator(user);
+
+      return Ok(new { asscess_token = Token });
+    }
+    catch (UnauthorizedException ex)
+    {
+      return Unauthorized(new { message = ex.Message });
+    }
+  }
+}
