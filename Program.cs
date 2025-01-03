2a18e664ed6f808af0652a2cb7f5d180ae1d310e
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-03 18:35:05 -0300

    feat: create filter for swagger bearer token

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
 using AuthBlog.Data;
+using AuthBlog.Filters;
 using AuthBlog.Repositories;
 using AuthBlog.Services.PostService;
 using AuthBlog.Services.UserService;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +18,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthBlog API", Version = "v1" });
+
+    // Definição de segurança JWT
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Por favor, insira o token JWT no campo abaixo (Bearer {token})",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.OperationFilter<AuthorizeCheckOperationFilter>();
+});
 
 builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
 builder.Services.AddDbContext<AppDbContext>(options =>
